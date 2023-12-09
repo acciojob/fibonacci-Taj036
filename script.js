40622433beb9bd6d854316f47e52dc381164861a
Taj036 <121137573+Taj036@users.noreply.github.com>
2023-12-09 16:11:48 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function fibonacci(num) {
-// your code here
-}
+function fibonacci(num, memo = {}) {
+  if (num === 0 || num === 1) {
+    return num;
+  }
 
+  if (memo[num]) {
+    return memo[num];
+  }
+
+  memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo);
+  return memo[num];
+}
 module.exports = fibonacci;
